7bcadefc61ba4a48de1ac7566638628cff934b3e
Carlos Boyanosky <carlos.boyanosky@gkco.com>
2022-05-12 08:37:09 -0300

    added messagequeue projects

---
+++ MessageQueue.Consumer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MessageQueue.Consumer
+{
+    public class Class1
+    {
+    }
+}
---
+++ MessageQueue.Producer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MessageQueue.Producer
+{
+    public class Class1
+    {
+    }
+}
