74ecbca43cc80a25ee4c1dc526fcda5730a18db4
Carlos Boyanosky <carlos.boyanosky@gkco.com>
2022-05-16 10:14:16 -0300

    added register capability

--- External.IdentityServerUI/Controllers/AuthController.cs
+++ External.IdentityServerUI/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using External.IdentityServerUI.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace External.IdentityServerUI.Controllers
@@ -8,11 +9,14 @@ namespace External.IdentityServerUI.Controllers
     public class AuthController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
 
         public AuthController(
+            UserManager<AppUser> userManager,
             SignInManager<AppUser> signInManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -33,6 +37,36 @@ namespace External.IdentityServerUI.Controllers
 
             ViewBag.Error = "Invalid credentials";
             return View(new LoginViewModel { ReturnUrl = loginVm.ReturnUrl });
+        }
+
+        [HttpGet]
+        public IActionResult Register(string returnUrl)
+        {
+            return View(new RegisterViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerVm);
+            }
+
+            var user = new AppUser();
+            user.UserName = registerVm.UserName;
+
+            var result = await _userManager.CreateAsync(user, registerVm.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddClaimAsync(user, new Claim("username", user.UserName));
+                await _signInManager.SignInAsync(user, false);
+                return Redirect(registerVm.ReturnUrl);
+            }
+
+            ViewBag.Error = "Invalid credentials";
+            return View(new LoginViewModel { ReturnUrl = registerVm.ReturnUrl });
         }
     }
 }
---
+++ External.IdentityServerUI/Models/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace External.IdentityServerUI.Models
+{
+    public class RegisterViewModel
+    {
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
